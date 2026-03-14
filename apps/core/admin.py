c12bdec77cd5fa213356fa13936743ee86d1a8d8
hannahashe <hannahashe13@gmail.com>
2026-03-14 20:28:23 +0000

    Remove SiteSettings model references from admin and context processors

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import SiteSettings
-
-
-@admin.register(SiteSettings)
-class SiteSettingsAdmin(admin.ModelAdmin):
-
-    list_display = ("site_name", "contact_email")
--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,10 +1,2 @@
-from .models import SiteSettings
-
-
 def site_settings(request):
-
-    settings = SiteSettings.objects.first()
-
-    return {
-        "site_settings": settings
-    }
+    return {}
