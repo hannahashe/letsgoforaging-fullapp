1f324149c5ebf4a0706f82723bce5e089256ed15
hannahashe <hannahashe13@gmail.com>
2026-03-14 19:56:46 +0000

    Add URL routing for home view and create home.html template

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+from django.shortcuts import render
 from django.urls import path
 
+
+def home(request):
+    return render(request, "home.html")
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home, name="home"),
 ]
