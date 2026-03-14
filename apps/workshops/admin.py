a15f2e5e1404a38db14dc4022c4351c69b87607b
hannahashe <hannahashe13@gmail.com>
2026-03-14 22:52:31 +0000

    Enhance WorkshopAdmin: Add image preview, update fieldsets, and improve list filters

--- apps/workshops/admin.py
+++ apps/workshops/admin.py
@@ -1,21 +1,96 @@
 from django.contrib import admin
 from .models import Workshop
+from django.utils.html import format_html
+
 
 
 @admin.register(Workshop)
 class WorkshopAdmin(admin.ModelAdmin):
 
     list_display = (
+        "image_preview",
         "title",
         "date",
+        "start_time",
         "location_name",
         "price",
         "spaces_available",
         "status",
     )
 
+    list_filter = (
+        "status",
+        "season",
+        "date",
+    )
+
+    search_fields = (
+        "title",
+        "location_name",
+    )
+
     prepopulated_fields = {"slug": ("title",)}
 
-    list_filter = ("status", "date")
+    ordering = ("date",)
 
-    search_fields = ("title", "location_name")
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+
+        ("Workshop Info", {
+            "fields": (
+                "title",
+                "slug",
+                "description",
+                "season",
+                "status",
+            )
+        }),
+
+        ("Schedule", {
+            "fields": (
+                "date",
+                "start_time",
+            )
+        }),
+
+        ("Location", {
+            "fields": (
+                "location_name",
+                "location_description",
+                "location_address",
+            )
+        }),
+
+        ("Booking", {
+            "fields": (
+                "price",
+                "max_participants",
+                "spaces_available",
+                "ticket_url",
+            )
+        }),
+
+        ("Media", {
+            "fields": (
+                "image",
+            )
+        }),
+
+        ("System", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+
+    )
+
+    def image_preview(self, obj):
+    if obj.image:
+        return format_html(
+            '<img src="{}" style="height:60px;border-radius:4px;" />',
+            obj.image.url
+        )
+    return "-"
+
