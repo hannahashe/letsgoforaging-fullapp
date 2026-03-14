3875710f46450221650aaaf37028d48cc5a86692
hannahashe <hannahashe13@gmail.com>
2026-03-14 22:20:17 +0000

    Implement WorkshopAdmin class in admin.py for enhanced model management

--- apps/workshops/admin.py
+++ apps/workshops/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Workshop
 
-# Register your models here.
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "date",
+        "location_name",
+        "price",
+        "spaces_available",
+        "status",
+    )
+
+    prepopulated_fields = {"slug": ("title",)}
+
+    list_filter = ("status", "date")
+
+    search_fields = ("title", "location_name")
