de6793a314b3c7ecf8d7ad548b3c77d3f0585f4e
hannahashe <hannahashe13@gmail.com>
2026-03-14 22:37:54 +0000

    Add upcoming workshops retrieval to home view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from apps.workshops.models import Workshop
 
 
 def home(request):
-    return render(request, "home.html")
+
+    upcoming_workshops = Workshop.objects.filter(
+        status="published"
+    ).order_by("date")[:3]
+
+    return render(
+        request,
+        "core/home.html",
+        {
+            "upcoming_workshops": upcoming_workshops
+        }
+    )
