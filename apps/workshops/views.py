6469622482b0dc57d077fe36e43ab1dfe32f0a8e
hannahashe <hannahashe13@gmail.com>
2026-03-14 22:35:07 +0000

    Add workshop list and detail view functions to handle published workshops

--- apps/workshops/views.py
+++ apps/workshops/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Workshop
 
-# Create your views here.
+
+def workshop_list(request):
+
+    workshops = Workshop.objects.filter(
+        status="published"
+        ).order_by("date")
+
+    return render(
+        request,
+        "workshops/workshop_list.html",
+        {"workshops": workshops}
+    )
+
+
+def workshop_detail(request, slug):
+
+    workshop = get_object_or_404(
+        Workshop,
+        slug=slug,
+        status="published"
+    )
+
+    return render(
+        request,
+        "workshops/workshop_detail.html",
+        {"workshop": workshop}
+    )
