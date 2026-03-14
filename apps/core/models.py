42ababbdd3a5acdcbeb00d0a632097fad64c0fb6
hannahashe <hannahashe13@gmail.com>
2026-03-14 20:25:06 +0000

    Remove SiteSettings model and simplify home view to render only the home page

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,19 +1,2 @@
 from django.db import models
 
-
-class SiteSettings(models.Model):
-
-    site_name = models.CharField(max_length=200)
-
-    hero_title = models.CharField(max_length=200)
-
-    hero_subtitle = models.TextField()
-
-    about_text = models.TextField()
-
-    contact_email = models.EmailField()
-
-    instagram_url = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.site_name
