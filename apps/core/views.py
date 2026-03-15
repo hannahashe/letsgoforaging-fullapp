ff5cafa65a984ce4c1b93d2a89c9a326b850b968
hannahashe <hannahashe13@gmail.com>
2026-03-15 17:39:38 +0000

    Add tag_page view to handle displaying workshops and blog posts by tag

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from apps.workshops.models import Workshop
+from taggit.models import Tag
+from apps.workshops.models import Workshop
+from apps.blog.models import BlogPost
 
 
 def home(request):
@@ -13,5 +16,23 @@ def home(request):
         "core/home.html",
         {
             "upcoming_workshops": upcoming_workshops
+        }
+    )
+
+
+def tag_page(request, slug):
+
+    tag = get_object_or_404(Tag, slug=slug)
+
+    workshops = Workshop.objects.filter(tags__slug=slug)
+    posts = BlogPost.objects.filter(tags__slug=slug)
+
+    return render(
+        request,
+        "core/tag_page.html",
+        {
+            "tag": tag,
+            "workshops": workshops,
+            "posts": posts
         }
     )
