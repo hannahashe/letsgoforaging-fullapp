67422f92d699b0efdbd5ea5e3d7c00e67e24d16f
hannahashe <hannahashe13@gmail.com>
2026-03-15 17:21:02 +0000

    Add TagAdmin class to manage Tag model in admin interface

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ("name",)
+
+    prepopulated_fields = {"slug": ("name",)}
