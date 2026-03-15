2c9d6357a0104866d4de2ef7f45d787856610f79
hannahashe <hannahashe13@gmail.com>
2026-03-15 18:27:37 +0000

    Add SiteSettings admin configuration for site management

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,10 +1,45 @@
 from django.contrib import admin
-from .models import Tag
+from .models import Tag, SiteSettings
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
 
     list_display = ("name",)
-
+    
     prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+
+        ("About", {
+            "fields": (
+                "about_text",
+                "about_image",
+            )
+        }),
+
+        ("Homepage", {
+            "fields": (
+                "hero_image",
+            )
+        }),
+
+        ("Contact", {
+            "fields": (
+                "contact_email",
+                "contact_phone",
+            )
+        }),
+
+        ("Social", {
+            "fields": (
+                "instagram",
+                "linktree",
+            )
+        }),
+
+    )
