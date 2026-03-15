185f0a62049a61ee1bebfc3c8ef52c51d5f84207
hannahashe <hannahashe13@gmail.com>
2026-03-15 18:57:26 +0000

    Add ReviewAdmin configuration for managing reviews in the admin panel

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from apps.reviews.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "workshop",
+        "rating",
+        "is_featured"
+    )
+
+    list_filter = ("rating", "is_featured")
