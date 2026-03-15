e10f305bb67177d986e1f2d103c990297e60d77b
hannahashe <hannahashe13@gmail.com>
2026-03-15 12:24:57 +0000

    Add GalleryImageAdmin for managing gallery images in the admin interface

--- apps/gallery/admin.py
+++ apps/gallery/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import GalleryImage
 
-# Register your models here.
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "image_preview",
+        "title",
+        "workshop",
+        "featured",
+        "created_at",
+    )
+
+    list_filter = (
+        "featured",
+        "workshop",
+    )
+
+    search_fields = (
+        "title",
+        "caption",
+    )
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="height:60px;border-radius:4px;" />',
+                obj.image.url
+            )
+        return "-"
